9a4d54f8718982be9ecd0cf2f6584d3d5046d9a2
calebTree <ande4905@gmail.com>
2021-12-25 20:24:47 -0600

    moved public/bike_ride.html to src/bike_ride.html, uses HtmlWebpackPlugin, and html-loader, splits entry chunks

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,23 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    bike: './src/bike_ride.html'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/bike_ride.html',
+      // inject: true,
+      chunks: ['bike', 'index'],
+      filename: 'bike_ride.html'
+      // filename: '[contenthash:6].html'
+    }),
+  ],
   output: {
-    filename: 'main.js',
+    filename: './bundles/[name].js',
     path: path.resolve(__dirname, 'public'),
     assetModuleFilename: 'images/[base]',
   },
@@ -17,6 +30,39 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+        options: {
+          sources: {
+            list: [
+              // All default supported tags and attributes
+              "...",
+              {
+                tag: "img",
+                attribute: "data-src",
+                type: "src",
+              },
+              {
+                tag: "img",
+                attribute: "data-srcset",
+                type: "srcset",
+              },
+            ],
+            urlFilter: (attribute, value, resourcePath) => {
+              // The `attribute` argument contains a name of the HTML attribute.
+              // The `value` argument contains a value of the HTML attribute.
+              // The `resourcePath` argument contains a path to the loaded HTML file.
+
+              if (/\.css$/i.test(value)) {
+                return false;
+              }
+
+              return true;
+            },
+          },
+        },
       },
     ],
   },
